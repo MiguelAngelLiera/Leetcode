08b98a530a58ff93c7ac98f514c190418a2e1206
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-05-27 12:22:23 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.42%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -5,12 +5,10 @@ class Solution:
         N = len(s)
         goods = 0
         while i <= N - 3:
-            sub = s[i:j]
-            if len(set(sub)) == 3:
+            if len(set(s[i:j])) == 3:
                 goods += 1
             i += 1
             j += 1
-
         return goods
 
             
