992af05c23d024e6d774c3dbe108af05d3ce3d60
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-06-19 10:28:03 -0600

    Time: 3 ms (40.14%) | Memory: 19.4 MB (28.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -2,12 +2,16 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
         res = [-1]*len(nums1)
-        s_nums1 = set(nums1)
-        for i, e in enumerate(nums2):
+        pos_nums1 = {}
+
+        for k, n in enumerate(nums1):
+            pos_nums1[n] = k
+        
+        for e in nums2:
             while len(stack) > 0 and stack[-1] < e:
                 last = stack.pop()
-                if last in s_nums1:
-                    idx = nums1.index(last)
+                idx = pos_nums1.get(last, None)
+                if idx is not None:
                     res[idx] = e
             stack.append(e)
 
