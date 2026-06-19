f1f3c1f4eba5e7e7a7d3f283de9d7131da5e2250
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-06-19 15:50:47 -0600

    Time: 3 ms (37.60%) | Memory: 20.1 MB (64.41%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -6,16 +6,12 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        return self.backtrack(root, targetSum)
-    
-    def backtrack(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return []
-        if not root.left and not root.right:
-            if root.val == targetSum:
-                return [[root.val]]
+        if not root.left and not root.right and root.val == targetSum:
+            return [[root.val]]
         paths = []
         for child in [root.left, root.right]:
-            paths += [[root.val] + path for path in self.backtrack(child, targetSum - root.val)]
+            paths += [[root.val] + path for path in self.pathSum(child, targetSum - root.val)]
         return paths
         
