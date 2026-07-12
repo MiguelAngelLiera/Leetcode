b25fe122c9cc825e2d6f0cebc61cd8025aef1e51
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-07-12 01:00:01 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (39.82%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
+        string = mapping[digits[0]] 
+        if len(digits) == 1:
+            return string
+        res = []
+        for c in string:
+            res += [c + tail for tail in self.letterCombinations(digits[1:])]
+        return res
+            
+        
