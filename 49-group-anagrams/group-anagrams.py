ddaaee245f6a317d0d1b5ab6ff7a922a1ab2a5b0
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-08-08 14:09:19 -0600

    Time: 160 ms (5.16%) | Memory: 22.7 MB (36.11%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,18 +1,20 @@
-from collections import Counter
+from collections import Counter, defaultdict
+from copy import deepcopy
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-        alpha = dict.fromkeys('abcdefghijklmnopqrstuvwxyz',0)
+        c = Counter({'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0})
+        #Counter()
+        groups = defaultdict(list)
         for s in strs:
-            ana = alpha.copy()
-            for c in s:
-                ana[c] += 1
-            ana = tuple(ana.values())
-            if ana in anagrams.keys():
-                anagrams[ana].append(s)
-            else:
-                anagrams[ana] = [s]
+            k = deepcopy(c)
+            k.update(s)
+            
+            k = str(list(k.values()))
+            groups[k].append(s)
+            del k
+        return list(groups.values())
+            
+            
         
-        return list(anagrams.values())
-
+        
         
