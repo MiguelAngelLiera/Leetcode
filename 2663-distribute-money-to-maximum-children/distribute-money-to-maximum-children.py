c99dc87be658920e595274015bbdbacc66357ed3
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-08-16 15:49:54 -0600

    Time: 1 ms (50.52%) | Memory: 19.4 MB (19.03%) - LeetSync

--- 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.py
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.py
@@ -1,26 +1,25 @@
 class Solution:
-    # money : 10, c = 4
-    # c1: 7, c2: 1, c3: 1, c4: 1
-    # money: 16 c = 4
-    # c1: 13, c2: 1, c3: 1, c4: 1
-    # c1: 8, money: 8, c= 3
-    ##
-    # money = 9,c = 2
-    # money = 12, c = 2, max_amo_f_child= 12-1: 11; money - 8 == 4
-    # money = 12, c =3
     def distMoney(self, money: int, children: int) -> int:
-        max_amo_f_child = money - (children - 1)
-        if money < children:
+        money -= children
+        if money < 0:
             return -1
-        if money < 8 or max_amo_f_child < 8:
-            return 0
-        if money - 8 == 4 and children - 1 == 1:
-            return 0
-        if children == 1 and money > 8:
-            return 0
-        rec = self.distMoney(money-8, children-1) # 9, 1
-        if rec == 0:
-            return 1
-        return 1 + rec
+
+        recieved = money // 7
+        residue = money % 7
+
+        if recieved == children and residue == 0:
+            return recieved
+
+        if recieved == children - 1 and residue == 3:
+            # 8, 8, 8, 4, recieved = 3, residue= 4
+            # 8, 8, 7, 5, recieved = 2, residue = 5
+            return recieved - 1
+
+        # money = 10, children = 5
+        # money = 5
+        # recived = 0, residue = 5
+        # children - 1
+        return min(children - 1, recieved)
+
 
         
